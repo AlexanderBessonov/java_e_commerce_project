9a9580f73e350a826ce7434dee876311e4025df9
Alexander Bessonov <bessonovUSA@gmail.com>
2022-08-10 20:34:59 -0500

    create Customer class

---
+++ src/company/main.java
@@ -0,0 +1,15 @@
+package company;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("");
+
+
+      //  createCystomer();
+      //  createCategore();
+      //  createProduct();
+       // createBalance();
+      //  createDiscount();
+    }
+
+}
